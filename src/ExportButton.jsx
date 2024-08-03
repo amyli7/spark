0ffc2680b2efb4c2b8ebe291eeff8b0fef91b277
yuhan <yuhanli27t@gmail.com>
2024-08-02 22:49:17 -0700

    font debugging

--- src/ExportButton.jsx
+++ src/ExportButton.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 export const ExportButton = ({ onClick }) => (
   <button
--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        '@fortawesome/react-fontawesome',
-        '@fortawesome/free-solid-svg-icons'
-      ],
-    },
-  },
-  server: {
-    port: 3000,
-  },
 });
